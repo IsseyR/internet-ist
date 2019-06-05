8bea6e684782265a98e603f548d8b91654567377
IsseyR <51253804+IsseyR@users.noreply.github.com>
2019-06-05 12:44:28 +1000

    Update reveal.js

--- reveal.js
+++ reveal.js
@@ -1,39 +1,44 @@
 // reveal on scroll
 ScrollReveal().reveal('.question', {
   delay: 200,
+  reset: true,
   origin: 'bottom',
   distance: '20px',
   viewOffset: {
     bottom: 100,
-    top: 50}}
+  }}
 );
 ScrollReveal().reveal('.questionAns', {
   delay: 200,
+  reset: true,
   origin: 'bottom',
   distance: '20px',
   viewOffset: {
     bottom: 100,
-    top: 50}}
+  }}
 );
 ScrollReveal().reveal('.descriptions', {
   delay: 200,
+  reset: true,
   origin: 'bottom',
   distance: '20px',
   viewOffset: {
     bottom: 100,
-    top: 50}}
+  }}
 );
 ScrollReveal().reveal('.internetDiagram', {
   delay: 200,
+  reset: true,
   scale: '0.7',
   viewOffset: {
     bottom: 100,
-    top: 50}}
+  }}
 );
 ScrollReveal().reveal('.internetYoutube', {
   delay: 200,
+  reset: true,
   scale: '0.7',
   viewOffset: {
     bottom: 300,
-    top: 50}}
+  }}
 );
